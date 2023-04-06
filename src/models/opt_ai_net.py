9efe826ab465240c8505bcd28a5371b860b41982
Pedro Luís Azevedo Costa <pdcost@icloud.com>
2023-04-06 11:52:17 -0300

    Supression function adjusted in Opt-AiNet

--- src/models/opt_ai_net.py
+++ src/models/opt_ai_net.py
@@ -20,12 +20,13 @@ class OptAiNet():
                  value_ranges:list,
                  fitness_func, # Function Type,
                  beta=100,
-                 clone_threshold = 0.1,
+                 clone_threshold = 0.01,
                  supression_threshold=0.2,
                  newcomers_percentage = 0.4,
                  seed=42,
                  eval_every=100,
                  verbose = 0,
+                 maintain_history = False
                 ):
         
         self.num_epochs = num_epochs
@@ -57,34 +58,47 @@ class OptAiNet():
         self.best_solution_fitness = 0
         self.best_solution = 0
 
+        self.maintain_history = maintain_history
+        self.memory_cell_history = []
+        self.clone_history = []
 
-        #self.init_pop()
-        #self.fitness_evaluation()
-        #self.clone()
-        #self.mutation()
-        #self.fitness_evaluation()
-        #self.evaluation()
-        #self.supress_cells()
-        #self.add_newcomers()
+        self.min_mat = self.value_ranges.T[0, :]
+        self.max_mat = self.value_ranges.T[1,:]
+        self.previous_memory_cells_quantity = 0
+        self.has_converged = False
+
+
+        # self.init_pop()
+        # self.fitness_evaluation()
+        # self.clone()
+        # self.mutation()
+        # self.fitness_evaluation()
+        # self.evaluation()
+        # #self.supress_cells()
+        # self.supress_cells_2()
+        # #self.add_newcomers()
 
 
     
     def init_pop(self):
         self.pop = np.random.rand(self.pop_size, self.chrom_length)
+        self.memory_cells = self.pop.copy()
         #self.min_mat = self.value_ranges.T[0, :]
         #self.max_mat = self.value_ranges.T[1,:]
         #self.pop = self.pop * (self.max_mat - self.min_mat) + self.min_mat
         #self.f_pop = self.fitness_func(self.pop)
     
     def fitness_evaluation(self):
-        self.f_pop = self.fitness_func(self.pop)
+        self.f_pop = self.fitness_func(self.pop, self.value_ranges)
         self.curr_f_max = self.f_pop.max()
         self.curr_f_min = self.f_pop.min()
 
-        if self.max_fitness < self.curr_f_max:
-            self.max_fitness = self.curr_f_max
-        if self.min_fitness > self.curr_f_min:
-            self.min_fitness = self.curr_f_min
+        self.max_fitness = self.curr_f_max
+        self.min_fitness = self.curr_f_min
+        # if self.max_fitness < self.curr_f_max:
+        #     self.max_fitness = self.curr_f_max
+        # if self.min_fitness > self.curr_f_min:
+        #     self.min_fitness = self.curr_f_min
 
         self.f_pop_norm = (self.f_pop - self.min_fitness)/(self.max_fitness - self.min_fitness)
         curr_max_fitness = self.f_pop.max()
@@ -97,6 +111,11 @@ class OptAiNet():
     def clone(self):
         self.pop = np.repeat(self.pop, repeats=self.Nc + 1, axis=0)
         self.f_pop_norm = np.repeat(self.f_pop_norm, repeats=self.Nc + 1, axis=0)
+        memory_denorm = self.memory_cells * (self.max_mat - self.min_mat) + self.min_mat
+        self.memory_cell_history.append(memory_denorm)
+        clone_denorm = self.pop * (self.max_mat - self.min_mat) + self.min_mat
+        self.clone_history.append(clone_denorm)
+
 
     def mutation(self):
         self.alpha = (1/self.beta) * np.exp(-self.f_pop_norm)
@@ -111,9 +130,16 @@ class OptAiNet():
 
         self.pop = self.pop + self.alpha * self.random_mutation
         mask = self.pop > 1
-        self.pop[mask] = 1
-        mask = self.pop < 0
-        self.pop[mask] = 0
+
+        # self.pop[mask] = 1
+        # print(mask)
+        # mask = self.pop < 0
+        # print(mask)
+        # self.pop[mask] = 0
+        rows_to_delete = np.any(self.pop > 1, axis=1)
+        self.pop = self.pop[~rows_to_delete]
+        rows_to_delete = np.any(self.pop < 0, axis=1)
+        self.pop = self.pop[~rows_to_delete]
 
         # Still needs to add a way to invalidate a individual in a positsion outside of the searhc spacie
 
@@ -122,13 +148,19 @@ class OptAiNet():
         mean_error = np.abs(self.f_pop_avg - self.f_pop_avg_previous)
         if  mean_error < self.clone_threshold:
             self.continue_clone = False
+            #if self.maintain_history:
+                #clone_denorm = self.pop * (self.max_mat - self.min_mat) + self.min_mat
+                #self.clone_history.append(clone_denorm)
         else:
             self.continue_clone = True
         self.f_pop_avg_previous = self.f_pop_avg
     
     def supress_cells(self):
-        distances = distance_matrix(self.pop, self.pop)        
+        print(self.pop)
+        distances = distance_matrix(self.pop, self.pop) 
+        print(distances)       
         f_pop_matrix = np.tile(self.f_pop, (distances.shape[0], 1)) 
+        print(f_pop_matrix)
         masked_f = f_pop_matrix * (distances<self.supression_threshold)
         best_indices = np.where(masked_f == masked_f.max(axis=1).T)[1]
         best_indices = np.unique(best_indices)
@@ -136,7 +168,60 @@ class OptAiNet():
         self.f_pop = self.f_pop[best_indices]
         self.best_ind = self.pop.copy()
         self.best_fits = self.f_pop.copy()
-    
+        self.memory_cells = self.pop.copy()
+
+    def supress_cells_2(self):
+        #print(self.pop)
+        #print(self.f_pop)
+        #distances = distance_matrix(self.pop, self.pop) 
+        #print(distances)
+
+        i = 0
+        j = 0
+        inter_count = 0
+        # print(self.pop)
+        # print("###")
+        # print(self.f_pop)
+        # print("###")
+        while True:
+            #if inter_count > 5:
+            #    return
+            #inter_count += 1
+            if i == j:
+                j+=1
+            if j >= self.pop.shape[0]:
+                i = i + 1 
+                j = 0
+            if i >= self.pop.shape[0]:
+                self.best_ind = self.pop.copy()
+                self.best_fits = self.f_pop.copy()
+                self.memory_cells = self.pop.copy()
+                break
+
+            dist = np.linalg.norm(self.pop[i] - self.pop[j])
+            is_near = dist < self.supression_threshold
+            if is_near:
+                if self.f_pop[i] > self.f_pop[j]:
+                    self.f_pop = np.delete(self.f_pop, j)
+                    self.pop = np.delete(self.pop, j, 0)
+                    #j+=1
+                else:
+                    self.f_pop = np.delete(self.f_pop, i)
+                    self.pop = np.delete(self.pop, i, 0)
+                    #i+=1
+            else:
+                j+=1
+
+        # Check if algorithm has converged
+        curr_memory_cells_quantity = self.pop.shape[0]
+        if self.previous_memory_cells_quantity == curr_memory_cells_quantity:
+            self.has_converged=True
+        else:
+            self.previous_memory_cells_quantity = curr_memory_cells_quantity
+        # print(self.pop)
+        # print("###")
+        # print(self.f_pop)
+                
     def add_newcomers(self):
         n_new_ind = int(len(self.pop) * self.newcomers_percentage)
         if n_new_ind == 0:
@@ -159,6 +244,8 @@ class OptAiNet():
         self.fitness_evaluation()
         for epoch in tqdm(range(self.num_epochs)):
             self.curr_epoch = epoch
+            ###
+            ###
             while self.continue_clone:
                 self.clone()
                 self.mutation()
@@ -166,10 +253,14 @@ class OptAiNet():
                 self.evaluation()
             self.continue_clone = True
             self.f_pop_avg_previous = 0
-            self.supress_cells()
+            self.supress_cells_2()
+            self.callback()
+            # print(self.previous_memory_cells_quantity)
+            # if self.has_converged:
+            #     print(f"Algorthm has converged in generation {epoch}")
+            #     break
             self.add_newcomers()
             self.fitness_evaluation()
-            self.callback()
         print("--- %s seconds ---" % (time.time() - start_time))
         return self.best_ind
 
@@ -180,3 +271,4 @@ class OptAiNet():
         plt.ylabel("Fitness Value")
         plt.legend()
         plt.show()
+
